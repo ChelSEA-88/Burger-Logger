6cd3dc4f57f7982677f66f8713e7da703f5e4bca
Chelsea Haviland <chelsea.haviland.13@gmail.com>
2020-09-26 17:26:09 -0400

    jawsdb

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,3 @@
-// const orm = require("../config/orm.js");
-
-// const burger = {
-//     selectAll: (cb) => {
-//         orm.selectAll("burgers", (res) => {
-//             cb(res);
-//         });
-//     },
-
-//     insertOne: (cols, vals, cb) => {
-//         orm.insertOne("burgers", cols, vals, (res) => {
-//             cb(res);
-//         });
-//     },
-
-//     updateOne: (id, obj, cb) => {
-//         //update burgers id condition (what is waiting to be devoured)
-//         const condition = "id = " + id;
-//         orm.updateOne("burgers", obj, condition, cb);
-//     },
-// };
-
-// module.exports = burger;
 
 // Import ORM object
 let orm = require("../config/orm.js");
